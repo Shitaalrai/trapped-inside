7bf24b772d71865d09d506abee12934acc2b0d8d
Bishuthapa <thapabishu435@gmail.com>
2026-05-24 14:13:25 +0545

    shital contributes

--- scripts/levels/level_2_map.gd
+++ scripts/levels/level_2_map.gd
@@ -14,11 +14,13 @@ var key_node: Area2D
 
 @onready var enemies_node: Node = $Enemies
 @onready var door: Area2D = $Door
+@onready var pause_menu = preload("res://UI/pause.tscn").instantiate()
 
 func _ready() -> void:
 	for enemy in enemies_node.get_children():
 		enemy.died.connect(_on_enemy_died)
-
+	add_child(pause_menu)
+	pause_menu.hide()
 
 var last_enemy_position: Vector2
 
@@ -37,6 +39,13 @@ func _process(_delta: float) -> void:
 	if enemies_node.get_child_count() == 0:
 		_spawn_key()
 
+func _input(event):
+	if event.is_action_pressed("ui_cancel"): # ESC
+		toggle_pause()
+
+func toggle_pause():
+	get_tree().paused = !get_tree().paused
+	pause_menu.visible = get_tree().paused
 
 func _spawn_key() -> void:
 	key_spawned = true
@@ -56,7 +65,6 @@ func _on_key_collected(_key: Area2D, _collector: Node2D) -> void:
 		return
 
 	has_key = true
-
 
 
 func _on_door_body_entered(body: Node2D) -> void:
