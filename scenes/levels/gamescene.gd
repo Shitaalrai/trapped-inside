e419d7e191813e1622719d07cad8e6ce0312d5c1
Bishuthapa <thapabishu435@gmail.com>
2026-05-24 16:30:35 +0545

    add the level 3 map

--- scenes/levels/gamescene.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-@onready var HUD: Control = $UI/HUD
-
-func _ready() -> void:
-	var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
-	player.update_hp_bar.connect(HUD.update_hp_bar)
-	
--- scenes/levels/hud.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-func update_hp_bar(new_value: int) -> void:
-	%HitpointsBar.value = new_value
-	
