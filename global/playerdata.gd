7f5b8d694b22a2fccca9bd5bdaae0d8ae7c3430e
Bishuthapa <thapabishu435@gmail.com>
2026-05-24 19:42:39 +0545

    add the player data to the root global folder

--- global/playerdata.gd
+++ global/playerdata.gd
@@ -1,11 +1,53 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+class_name PlayerDataService
+
+signal lives_changed(lives: int)
+signal hp_changed(hitpoints: int, hitpoint_max: int)
+signal game_over()
+
+const MAX_LIVES: int = 3
+const MAX_HP: int = 200
+
+var lives: int = MAX_LIVES
+var hitpoints: int = MAX_HP
+var lost_life_in_room: bool = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func reset_for_new_game() -> void:
+	lives = MAX_LIVES
+	hitpoints = MAX_HP
+	lost_life_in_room = false
+	lives_changed.emit(lives)
+	hp_changed.emit(hitpoints, MAX_HP)
+
+
+func apply_room_transition() -> void:
+	if not lost_life_in_room:
+		hitpoints = MAX_HP
+	else:
+		hitpoints = MAX_HP
+	lost_life_in_room = false
+	hp_changed.emit(hitpoints, MAX_HP)
+
+
+func take_damage(amount: int) -> void:
+	if amount <= 0:
+		return
+	hitpoints = maxi(hitpoints - amount, 0)
+	hp_changed.emit(hitpoints, MAX_HP)
+
+
+func lose_life() -> bool:
+	lost_life_in_room = true
+	lives -= 1
+	lives_changed.emit(lives)
+	if lives <= 0:
+		hitpoints = 0
+		hp_changed.emit(hitpoints, MAX_HP)
+		game_over.emit()
+		return false
+	hitpoints = MAX_HP
+	hp_changed.emit(hitpoints, MAX_HP)
+	return true
